345d98b91f3bc355b2da04036443effe6a57cb70
smnfive <121691503+smnfive@users.noreply.github.com>
2023-03-04 23:12:56 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from my_token import TOKEN
+import requests
+
+class VK:
+
+   def __init__(self, access_token, user_id, version='5.131'):
+       self.token = access_token
+       self.id = user_id
+       self.version = version
+       self.params = {'access_token': self.token, 'v': self.version}
+
+   def users_info(self):
+       url = 'https://api.vk.com/method/users.get'
+       params = {'user_ids': self.id}
+       response = requests.get(url, params={**self.params, **params})
+       return response.json()
+
+
+access_token = TOKEN
+user_id = 264521131
+vk = VK(access_token, user_id)
+print(vk.users_info())
