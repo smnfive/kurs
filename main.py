ca38868a21490e807ea13ed7c29d74fa222e0b64
smnfive <121691503+smnfive@users.noreply.github.com>
2023-03-05 00:05:20 +0300

    class Ya

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from my_token import TOKEN
+from my_token import TOKEN_VK
+from my_token import TOKEN_YA
 import requests
 from pprint import pprint
 
@@ -23,8 +24,40 @@ class VK:
         response = requests.get(url, params=params)
         return response.json()
 
+class YandexDisk:
 
-access_token = TOKEN
+    def __init__(self, token):
+        self.token = token
+
+    def make_upload_link(self, name_your_path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": name_your_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        return response.json()['href']
+
+    def upload(self, name_your_path, url_photo_vk):
+        href = self.make_upload_link(name_your_path=name_your_path)
+        response = requests.put(href, data=url_photo_vk)
+        response.raise_for_status()
+        if response.status_code == 201:
+            print('Success')
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
+
+    def get_files_list(self):
+        files_url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
+        headers = self.get_headers()
+        response = requests.get(files_url, headers=headers)
+        return response.json()
+
+access_token = TOKEN_VK
+token = TOKEN_YA
 user_id = 264521131
 vk = VK(access_token, user_id)
+ya = YandexDisk(token)
 pprint(vk.get_photo())
